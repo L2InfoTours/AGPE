320074227a43441ebf12ac55122ff65633a23bea
Modan <mdan45941@gmail.com>
2021-01-02 18:27:32 +0100

    correction

--- src/gui/page/TimeExamElement.java
+++ src/gui/page/TimeExamElement.java
@@ -4,8 +4,11 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
+import JFX.mote.controls.Select;
+import JFX.mote.controls.Spinner;
 import JFX.mote.controls.TextField;
 import JFX.mote.controls.TimetableElement;
+import JFX.mote.controls.Tree;
 import JFX.mote.layout.Popup;
 
 public class TimeExamElement extends TimetableElement {
@@ -22,6 +25,9 @@ public class TimeExamElement extends TimetableElement {
 		System.out.println("Open");
 		Popup pop = new Popup("Ajouter un Examen");
 		pop.add(new TextField("Nom"));
+		pop.add(new Spinner(0, 0, 0, 0));
+		pop.add(new Select());
+		pop.add(new Tree());
 		pop.open();
 	}
 	
