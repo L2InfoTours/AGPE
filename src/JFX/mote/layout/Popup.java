7854e9762b61f00347ed46fcda66465b04bc352b
Modan <mdan45941@gmail.com>
2021-01-03 23:11:50 +0100

    scroll popup

--- src/JFX/mote/layout/Popup.java
+++ src/JFX/mote/layout/Popup.java
@@ -8,6 +8,7 @@ import JFX.mote.components.Title;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -24,6 +25,7 @@ import javafx.stage.StageStyle;
 public class Popup {
 	private Layout content  = new Layout();
 	private Stage pop;
+	private ScrollPane scroll;
 	/**
 	 * 
 	 * @param name
@@ -32,7 +34,7 @@ public class Popup {
 		pop=new Stage();
 		pop.initModality(Modality.APPLICATION_MODAL);
 		pop.setTitle(name);
-
+		scroll = new ScrollPane();
 		VBox layout= new VBox();
 		content.setMode(Layout.DARKMODE);
 		content.setDisplay(LayoutDisplay.Column);
@@ -56,10 +58,11 @@ public class Popup {
 		content.init();
 		content.setMinSize(App.width*.8, App.height*.8);
 		content.setMaxSize(App.width*.8, App.height*.8);
-		content.setAlignment(Pos.TOP_CENTER);
+		//content.setAlignment(Pos.TOP_CENTER);
 		
 		
-		layout.getChildren().addAll(top,content);   
+		layout.getChildren().addAll(top,scroll);   
+		scroll.setContent(content);
 	     
 		layout.setClip(rect);
 		Scene sc= new Scene(layout, App.width*.8, App.height*.8);
