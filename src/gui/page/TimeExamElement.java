20f16e6065d505e71a0f61ea8247a2805e74a87d
Modan <mdan45941@gmail.com>
2021-01-04 10:23:36 +0100

    creation panel

--- src/gui/page/TimeExamElement.java
+++ src/gui/page/TimeExamElement.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import JFX.mote.controls.Select;
+import JFX.mote.controls.TimePicker;
 import JFX.mote.controls.TimetableElement;
 import JFX.mote.controls.Tree;
 import JFX.mote.controls.TreeItem;
@@ -22,22 +23,7 @@ public class TimeExamElement extends TimetableElement {
 		this.topic = topic;
 	}
 	public void createPanel(){
-		System.out.println("Open");
-		Popup pop = new Popup("Ajouter un Examen");
-	//	pop.add(new TextField("Nom"));
-//		pop.add(new Spinner(0, 0, 0, 0));
-		pop.add(new Select(Arrays.asList("Aa","Bb","Cc")));
-		Tree a = new Tree();
-		a.add("A1");
-		a.add("A2");
-		a.add("A3");
-		TreeItem b = a.createSection("élèves");
-		b.add("A");
-		b.add("B");
-		b.add("C");
-		pop.add(a);
-		//pop.add(new TimePicker());
-		pop.open();
+		(new CreationPanel()).open();
 	}
-	
+
 }
