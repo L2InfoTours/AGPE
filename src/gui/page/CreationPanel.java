d2304abdfc2eac11156893ebd55692199cbe7a20
Modan <mdan45941@gmail.com>
2021-01-04 10:35:04 +0100

    corretion

--- src/gui/page/CreationPanel.java
+++ src/gui/page/CreationPanel.java
@@ -8,32 +8,21 @@ import JFX.mote.controls.Select;
 import JFX.mote.controls.TimePicker;
 import JFX.mote.controls.Tree;
 import JFX.mote.controls.TreeItem;
+import JFX.mote.layout.Form;
 import JFX.mote.layout.Popup;
 
-public class CreationPanel extends Popup{
+public class CreationPanel extends Form{
 	private Select topic;
 	private Tree eleveTree;
 	private TimePicker time;
 	public CreationPanel() {
-		super("Ajouter un Examen");
+		super("Ajouter un examen",null);
 		topic = new Select();
 		time = new TimePicker();
 		eleveTree = new Tree();
 		add(topic);
 		add(time);
 		add(eleveTree);
-		Tree a = new Tree();
-		TreeItem b = a.createSection("L2 Math");
-		b.add("Jean");
-		b.add("Batiste");
-		b.add("Cory");
-		TreeItem c = a.createSection("L2 Info");
-		c.add("Avrelle");
-		c.add("Dorien");
-		c.add("Elza");
-		add(a);
-
-		open();
 	}
 	public void setPromo(HashMap<String,List<Object>> promos){
 		eleveTree.clear();
